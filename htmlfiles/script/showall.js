f8efd2866d85622089f87fe62bc23eeb85ef3a0f
CarstenHa <git@langstreckentouren.de>
2022-12-11 23:31:12 +0100

    Projekt eingestellt.

--- htmlfiles/script/showall.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-	  document.getElementById('button').addEventListener('click',displayblock);
-    function displayblock () {
-      for (var i = 0, div = document.getElementsByClassName('masterroute'); i < div.length; i++) {
-        div[i].style.display = 'block';
-      }
-    }
-});
